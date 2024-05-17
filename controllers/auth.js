2097c9a7bfc1f4425b09e6e56608a40d30cd905f
Shivendra490 <shivendrasrivastava490@gmail.com>
2024-05-17 20:16:04 +0530

    added folders struct  and added signup route

---
+++ controllers/auth.js
@@ -0,0 +1,47 @@
+const User = require("../models/user");
+
+const bcrypt = require("bcrypt");
+
+exports.signUp = async (req, res, next) => {
+  try {
+    const { email, name, password, mobile } = req.body;
+    if (
+      !email?.trim() ||
+      !name?.trim() ||
+      !password?.trim() ||
+      !mobile?.trim()
+    ) {
+      const error = new Error("All field are required");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const isUserExist = await User.findOne({ email: email });
+    if (isUserExist) {
+      return res.json({ message: "user is already registered" });
+    }
+    const hashedPwd = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      email,
+      name,
+      password: hashedPwd,
+      mobile,
+    });
+    
+    res
+      .status(201)
+      .json({
+        message: "user registered successfully",
+        user: { email: newUser.email, _id: newUser._id, name: newUser.name },
+      });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.login = (req, res, next) => {
+  res.send("login");
+};
--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const express=require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app=express()
+const dotenv = require("dotenv");
+const cors=require("cors")
+const bodyParser=require("body-parser")
+dotenv.config();
 
-app.get('/',(req,res,next)=>{
-    res.send('home route')
+const authRoutes=require("./routes/auth")
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+app.get("/", (req, res, next) => {
+  res.send("home route");
+});
+
+app.use('/api/auth',authRoutes)
+
+app.use((err,req,res,next)=>{
+    const status=err.statusCode || 500
+    console.log('here err',err)
+    return res.status(status).json({message:err.message})
 })
 
-
-app.listen(8080,()=>console.log("server is running at port 8080"))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Server is UP at port ", process.env.PORT);
+    app.listen(process.env.PORT);
+  })
+  .catch((err) => console.log("mongoose catch ERROR", err));
