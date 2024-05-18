1513afba6d22bc0b18236c4efab09a0acd19f693
Shivendra490 <shivendrasrivastava490@gmail.com>
2024-05-18 22:05:02 +0530

    loginroute ,send jsontoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 
 const bcrypt = require("bcrypt");
+const jwt=require("jsonwebtoken")
 
 exports.signUp = async (req, res, next) => {
   try {
@@ -27,13 +28,11 @@ exports.signUp = async (req, res, next) => {
       password: hashedPwd,
       mobile,
     });
-    
-    res
-      .status(201)
-      .json({
-        message: "user registered successfully",
-        user: { email: newUser.email, _id: newUser._id, name: newUser.name },
-      });
+
+    res.status(201).json({
+      message: "user registered successfully",
+      user: { email: newUser.email, _id: newUser._id, name: newUser.name },
+    });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -42,6 +41,33 @@ exports.signUp = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send("login");
+exports.login = async (req, res, next) => {
+  // res.send("login");
+  try {
+    const { email, password } = req.body;
+    if (!email?.trim() || !password?.trim()) {
+      const error = new Error("All field are required");
+      error.statusCode = 422;
+      throw error;
+    }
+    const user=await User.findOne({email})
+    if(!user){
+      const error = new Error("Credentials are invalid");
+      error.statusCode = 401;
+      throw error;
+    }
+    const isEqual=await bcrypt.compare(password,user.password)
+    if(!isEqual){
+      const error = new Error("Credentials are invalid,unauthorized access");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token=jwt.sign({email:user.email,userId:user._id.toString()},'supersecretkey',{expiresIn:'1h'})
+    res.status(200).json({message:"login successful",token:token,userId:user._id,email:user.email})
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
